c55e6e1aa39ab5743504b30e195e0baa3b63fa68
kunaljx86 <mail.kunaljoshi@gmail.com>
2020-09-20 16:02:00 +0530

    Removed unnecessary import

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
 const MenuItem = ({ dish, onClick }) => {
